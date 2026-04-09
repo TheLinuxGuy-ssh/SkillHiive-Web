60eccbf4cb227b970d90471f80b2cc0451a02a54
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2026-04-09 13:09:15 +0530

    search bar fixed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,12 +5,13 @@ const Nav = () => {
         <div className="nav fixed bottom-4 left-0 w-full flex justify-center">
             <div className="wrapper rounded-2xl flex p-1 bg-white/10  justify-center">
                 <div className="nav-left  flex justify-start items-center">
-                    <div className="nav-brand text-2xl font-bold ml-2 mr-10 text-[#fffd01]">
+                    <div className="nav-brand text-2xl font-bold ml-2 mr-2 text-[#fffd01]">
                         <img alt="" />
                         SkillHive
                     </div>
-                    <div className="nav-skill-search-box">
-                        <input type="text" placeholder='Explore...' className="search-box transition-ui w-2 focus:w-auto border-1 border-white/10   outline-0 bg-white/5 focus:bg-white/15 rounded-2xl px-3 py-2" />
+                    <div className="nav-skill-search-box relative w-auto flex items-center">
+                        <input type="text" placeholder='Explore...' className="search-box w-9.5 text-transparent focus:text-white placeholder-transparent focus:placeholder-grey-100 focus:w-50 transition-ui border-1 border-white/10   outline-0 bg-white/5 focus:bg-white/15 rounded-full px-3 py-2" />
+                        <i className='fa fa-search absolute right-2.5 pointer-events-none'></i>
                     </div>
                 </div>
                 <div className="nav-center  flex justify-center">
